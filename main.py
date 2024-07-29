23432f19d6b7ae7355768d5ccedb801cbdf847bf
nati <nwtbekele@gmail.com>
2024-07-28 21:42:18 -0500

    get packages, get licenses, get package requirements

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
 import subprocess
+import sys
 from importlib.metadata import distribution
 
-dependencies = subprocess.Popen(["pip", "freeze"], stdout=subprocess.PIPE)
-dependencies = dependencies.stdout.read().decode("utf-8")
-dependencies_list = dependencies.split("\n")
-package_list = [package.split("==")[0] for package in dependencies_list if package]
+# dependencies = subprocess.Popen(["pip", "freeze"], stdout=subprocess.PIPE)
+# dependencies = dependencies.stdout.read().decode("utf-8")
+common_licenses = [
+    "MIT",
+    "Apache",
+    "BSD",
+    "GPL",
+    "LGPL",
+]
 
-print(package_list)
 
-licenses = {package:distribution(package).metadata['License'] for package in package_list}
-other2= {package:distribution(package).metadata.get_all('Classifier') for package in package_list}
-other = {package:distribution(package).metadata['PKG-INFO'] for package in package_list}
-#print(licenses)
-#print(other)
-print(other2)
+def get_dependencies() -> list[str]:
+    dependencies = subprocess.check_output(["pip3", "freeze"]).decode("utf-8")
+    dependencies = [dep.split("==")[0] for dep in dependencies.split("\n") if dep]
+    return dependencies
+
+
+def get_package_requirements(package_name: str) -> list[str]:
+    package_info = subprocess.check_output(["pip3", "show", package_name]).decode(
+        "utf-8"
+    )
+
+    for info in package_info.split("\n"):
+        if info.startswith("Requires"):
+            package_requirements = info.split(": ")[1].split(", ")
+            break
+    return package_requirements
+
+
+def get_license(package: str) -> str:
+    if not (license := distribution(package).metadata["License"]) or len(license) > 10:
+        for value in distribution(package).metadata.get_all("Classifier"):
+            if "License" in value:
+                license = value.split(" :: ")[-1]
+                break
+    return license.replace("License", "").strip() if license else "?"
+
+
+def get_licenses():
+    dependencies = get_dependencies()
+    licenses = {}
+
+    for package in dependencies:
+        print(f"{package}: {get_license(package)}")
+        licenses[package] = get_license(package)
+
+        print(f"{package}: {get_package_requirements(package)}")
+
+
+get_licenses()
