f4243c13418ffb30b4e3785c701f2bae762de35b
nati <nwtbekele@gmail.com>
2024-07-29 20:44:48 -0500

    OOP structuring

--- main.py
+++ main.py
@@ -2,77 +2,94 @@ import re
 import subprocess
 from importlib.metadata import distribution
 
-# dependencies = subprocess.Popen(["pip", "freeze"], stdout=subprocess.PIPE)
-# dependencies = dependencies.stdout.read().decode("utf-8")
-common_licenses = [
-    "MIT",
-    "Apache",
-    "BSD",
-    "GPL",
-    "LGPL",
-]
+
+class Package:
+    def __init__(self, name: str, license: str) -> None:
+        self.name = name
+        self.license = license
+
+    @property
+    def requirements(self) -> list[str]:
+        return self._requirements
+
+    @requirements.setter
+    def requirements(self, reqs: list[str]) -> None:
+        self._requirements = reqs
 
 
-def get_dependencies() -> list[str]:
-    dependencies = subprocess.check_output(["python", "-m" "pip", "freeze"]).decode(
-        "utf-8"
-    )
-    dependencies = [dep.split("==")[0] for dep in dependencies.split("\n") if dep]
-    return dependencies
+class ProjectLicenses:
+    def __init__(self, depth: int) -> None:
+        self._depth = depth
+        self._package_libraries: dict[str, Package] = {}  # map package name to object
+        self._procjet_dependencies: list[str] = (
+            []
+        )  # direct dependencies names of project
 
+    def find_project_dependencies(self) -> list[str]:
+        dependencies = subprocess.check_output(["python", "-m" "pip", "freeze"]).decode(
+            "utf-8"
+        )
+        dependencies = [dep.split("==")[0] for dep in dependencies.split("\n") if dep]
+        return dependencies
 
-print(distribution("pandas").metadata.get_all("Requires-Dist"))
+    def get_package_requirements(self, package_name: str) -> list[str]:
+        package_requirements = []
+        if req_info := distribution(package_name).metadata.get_all("Requires-Dist"):
+            package_requirements = [
+                re.split(r"[<>=~\(;!]", req)[0].strip()
+                for req in req_info
+                if ";" not in req or "; python_version" in req
+                # requirements for only certain python versions are noted with ; python_version
+            ]
+        print(package_requirements)
+        return package_requirements
 
+    def get_license(self, package: str) -> str:
+        if package in self._package_libraries:
+            return self._package_libraries[package].license
 
-def get_package_requirements(package_name: str) -> list[str]:
-    # pip show is slow in some cases since license outputs are long
-    # package_info = subprocess.check_output(
-    #     ["python", "-m", "pip", "show", package_name]
-    # ).decode("utf-8")
+        if (
+            not (license := distribution(package).metadata["License"])
+            or len(license) > 10
+        ):
+            classifier = distribution(package).metadata.get_all("Classifier")
+            if not classifier:
+                # edge case when package does not have classifier information
+                return "?"
 
-    # for info in package_info.split("\n"):
-    #     if info.startswith("Requires"):
-    #         package_requirements = info.split(": ")[1].split(", ")
-    #         break
+            for value in classifier:
+                if "License" in value:
+                    license = value.split(" :: ")[-1]
+                    break
+        return license.replace("License", "").strip() if license else "?"
 
-    package_requirements = []
-    if req_info := distribution(package_name).metadata.get_all("Requires-Dist"):
-        package_requirements = [
-            re.split(r"[<>=~\(;]", req)[0].strip()
-            for req in req_info
-            if ";" not in req or "; python_version" in req
-            # requirements for only certain python versions are noted with ; python_version
-        ]
+    def get_project_dependencies_and_licenses(self):
+        dependencies = self.find_project_dependencies()
+        for package in dependencies:
+            print(f"{package}: {self.get_license(package)}")
+            cur_package = Package(package, self.get_license(package))
+            cur_package.requirements = self.get_package_requirements(package)
 
-    #    print(
-    #        set(other_req),
-    #        package_requirements,
-    #        len(list(set(other_req))) == len(package_requirements),
-    #    )
-    #
-    print(package_requirements)
-    return package_requirements
+            self._package_libraries[package] = cur_package
+            self._procjet_dependencies.append(package)
 
+    def fetch_recursive_dependencies(self):
+        queue = self._procjet_dependencies[:]
 
-def get_license(package: str) -> str:
-    if not (license := distribution(package).metadata["License"]) or len(license) > 10:
-        for value in distribution(package).metadata.get_all("Classifier"):
-            if "License" in value:
-                license = value.split(" :: ")[-1]
-                break
-    return license.replace("License", "").strip() if license else "?"
+        while queue:
+            package = queue.pop()
 
+            if not package in self._package_libraries:
+                cur_package = Package(package, self.get_license(package))
+                cur_package.requirements = self.get_package_requirements(package)
+                self._package_libraries[package] = cur_package
 
-def get_licenses():
-    dependencies = get_dependencies()
-    licenses = {}
+            for req in self._package_libraries[package].requirements:
+                if req not in self._package_libraries:
+                    queue.append(req)
 
-    for package in dependencies:
-        print(f"{package}: {get_license(package)}")
-        licenses[package] = get_license(package)
-        get_package_requirements(package)
+    def pretty_print(self):
+        pass
 
-
-get_licenses()
 
 # depth of recursion with -r flag
