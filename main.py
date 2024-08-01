f65dd0750bd4ea73c7ae6713409a0bf73d59d920
nati <nwtbekele@gmail.com>
2024-07-31 22:02:30 -0500

    Fix version matching of package req dependency and project python version

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import platform
 import re
 import subprocess
-from importlib.metadata import PackageNotFoundError, distribution
+from collections import Counter
+from importlib.metadata import distribution
 
 
 class Package:
@@ -23,7 +24,7 @@ class Package:
 
 class ProjectLicenses:
     def __init__(self, depth: int) -> None:
-        self._python_version = platform.python_version()
+        self._python_version = platform.python_version_tuple()
         self._depth = depth
         self._package_libraries: dict[str, Package] = {}  # map package name to object
         self._project_dependencies: list[str] = (
@@ -31,94 +32,86 @@ class ProjectLicenses:
         )  # direct dependencies names of project
 
     def find_project_dependencies(self) -> list[str]:
-        dependencies = subprocess.check_output(["python", "-m" "pip", "freeze"]).decode(
-            "utf-8"
+        dependencies = subprocess.check_output(
+            ["python", "-m" "pip", "freeze"], text=True
         )
         dependencies = [dep.split("==")[0] for dep in dependencies.split("\n") if dep]
         return dependencies
 
-    def _matching_version(self, req_info: str) -> bool:
+    def _matches_python_version(self, req_info: str) -> bool:
+        """Check if the python version for which requirement of a package is needed matches the
+        projects python version."""
+
         expression = req_info.split(";")[1]
-        version = re.split(r"[<>=]", expression)[1].strip().split(".")
+
+        for char in ["'", '"', " "]:
+            expression = expression.replace(char, "")
+
+        version = re.split(r"==|<=|>=|!=|<|>", expression)[1].strip().split(".")
         if len(version) == 2:
-            version.append("0")
-        
-        diff = [int(ver) - int(p_ver) for ver, p_ver in zip(version, self._python_version)]
-        print(diff)
+            version.append(self._python_version[2])
+
+        diff = [
+            int(ver) - int(p_ver) for ver, p_ver in zip(version, self._python_version)
+        ]
+
+        # print(req_info, expression, version, self._python_version, diff, end=" -> ")
         if "<=" in expression:
             return diff[0] > 0 or (diff[0] == 0 and diff[1] >= 0)
         elif "<" in expression:
             return diff[0] > 0 or (diff[0] == 0 and diff[1] > 0)
         elif ">=" in expression:
-            return diffqqqqq q
+            return diff[0] < 0 or (diff[0] == 0 and diff[1] <= 0)
         elif ">" in expression:
-
+            return diff[0] < 0 or (diff[0] == 0 and diff[1] > 0)
         elif "==" in expression:
-
+            return diff[0] == 0 and diff[1] == 0 and diff[2] == 0
         elif "!=" in expression:
-
-        else:
-            print(expression)
+            return diff[0] != 0 or diff[1] != 0 or diff[2] != 0
         return True
 
     def get_package_requirements(self, package_name: str) -> list[str]:
         package_requirements = []
-        try:
-            if req_info := distribution(package_name).metadata.get_all("Requires-Dist"):
-                for req in req_info:
-                    if ";" not in req or ("; python_version" in req) and self._matching_version(req):
-                        package_requirements.append(re.split(r"[<>=~\(;!]", req)[0].strip())
-                # package_requirements = [
-                #     re.split(r"[<>=~\(;!]", req)[0].strip()
-                #     for req in req_info
-                #     if ";" not in req
-                #     or "; python_version" in req
-                #     and self._matching_version(req)
-                #     # check the version and if it matches the current one
-                #     # requirements for only certain python versions are noted with ; python_version
-                # ]
-        except PackageNotFoundError:
-            return []
+        if req_info := distribution(package_name).metadata.get_all("Requires-Dist"):
+            for req in req_info:
+                if (
+                    ";" not in req
+                    or ("; python_version" in req)
+                    and self._matches_python_version(req)
+                ):
+                    package_req = re.split(r"[<>=~\(;!]", req)[0].strip()
+                    package_requirements.append(package_req)
 
         return package_requirements
 
     def get_license(self, package: str) -> str:
-        license = None
+        """Get the license of the package from the cache, License metadata, or Classifier metadata."""
+
         if package in self._package_libraries:
             return self._package_libraries[package].license
-        try:
-            if (
-                not (license := distribution(package).metadata["License"])
-                or len(license) > 10
-            ):
-                classifier = distribution(package).metadata.get_all("Classifier")
-                if not classifier:
-                    # edge case when package does not have classifier information
-                    return "?"
 
-                for value in classifier:
-                    if "License" in value:
-                        license = value.split(" :: ")[-1]
-                        break
-        except:
-            try:
-                classifier = distribution(package).metadata.get_all("Classifier")
-                if not classifier:
-                    # edge case when package does not have classifier information
-                    return "?"
+        if (
+            not (license := distribution(package).metadata["License"])
+            or len(license) > 10
+        ):
+            # really long license strings are likely to be the entire licensing doc
 
-                for value in classifier:
-                    if "License" in value:
-                        license = value.split(" :: ")[-1]
-                        break
-            except:
-                pass
-        return license.replace("License", "").strip() if license else "?"
+            classifier = distribution(package).metadata.get_all("Classifier")
+            if not classifier:
+                # edge case when package does not have classifier information
+                return "?"
+
+            for value in classifier:
+                if "License" in value:
+                    license = value.split(" :: ")[-1]
+                    return license.replace("License", "").strip()
+
+        return license.replace("License", "").strip()
 
     def get_project_dependencies_and_licenses(self):
         dependencies = self.find_project_dependencies()
         for package in dependencies:
-            print(package)
+            # print(package)
             cur_package = Package(package, self.get_license(package))
             cur_package.requirements = self.get_package_requirements(package)
 
@@ -144,12 +137,13 @@ class ProjectLicenses:
         pass
 
     def pretty_print(self):
-        print(self._package_libraries)
+        # print(self._package_libraries)
         for p, pack in self._package_libraries.items():
             print(pack)
-            print(f"\treqs -> {pack.requirements}")
+            if pack.requirements:
+                print(f"\treqs -> {pack.requirements}")
 
-        print(self._project_dependencies)
+        # print(self._project_dependencies)
 
 
 if __name__ == "__main__":
