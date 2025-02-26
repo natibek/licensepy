28596495120d4b68a8848209a9d2ee45db8d7200
nati b <nwtbekele@gmail.com>
2025-02-25 18:02:46 -0600

    get the package directories, find metadata files, start parsing for license and requirements

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,81 @@
 use std::fs::{File, read_dir, DirEntry};
 use std::io::{self, BufRead};
-use std::fmt::Debug;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
-struct MetaData {
+#[derive(Default, Debug)]
+struct Metadata {
     name: String,
-    license: String,
+    license: Vec<String>,
     requirements: Vec<String>,
 }
+
 #[derive(Debug)]
-enum DistType<PathBuf> {
+enum DistType {
     EggDir(PathBuf),
     DistDir(PathBuf),
     Info(PathBuf),
 }
 
-fn main() {
-    let dist_dirs = get_dist_directories();
-    let package_dirs = get_package_directories(dist_dirs);
+impl DistType {
+    fn get_metadata(self) -> Metadata {
+        match self {
+            DistType::EggDir(path) => {
+                let metadata_path = path.join("PKG-INFO");
+                parse_metadata(metadata_path)
+            },
+            DistType::DistDir(path) => {
+                let metadata_path = path.join("METADATA");               
+                parse_metadata(metadata_path)
+            },
+            DistType::Info(path) => parse_metadata(path)
+        }
+    }
 }
 
-fn get_package_directories(dist_dirs: Vec<String>) -> Vec<DistType<PathBuf>> {
-// directory needs to end with .egg-info with PKG_INFO or .dist-info with METADATA
+fn main() {
+    let dist_dirs = get_dist_directories();
+    println!("{:?}", dist_dirs);
+    let package_dist: Vec<DistType> = dist_dirs
+                    .into_iter()
+                    .flat_map(get_package_dir)
+                    .collect();
+    println!("{:?}", package_dist);
+    for dist in package_dist{
+        println!("{:?}", dist);
+        let metadata = dist.get_metadata();
+        println!("{:?}", metadata);
+        println!();
+    }
+}
+
+fn get_package_dir(dist_dir: String) -> Vec<DistType> {
+// directory needs to end with .egg-info with PKG-INFO or .dist-info with METADATA
 // or the info file
-    let mut package_dirs: Vec<DistType<PathBuf>> = Vec::new();
-    for dir in dist_dirs {
-        let package_dir: Vec<DistType<PathBuf>>= match read_dir(dir) {
+        match read_dir(dist_dir) {
             Err(why) => panic!("Failed to read directory {}", why),
             Ok(files) => files
-                            .filter_map(|entry| entry.ok())
-                            .filter_map(|entry: DirEntry| {
-                                let path = entry.path();
-                                let filename = entry.file_name().into_string().ok()?;
+                        .filter_map(|entry| entry.ok())
+                        .filter_map(|entry: DirEntry| {
+                            let path = entry.path();
+                            let filename = entry.file_name().into_string().ok()?;
 
-                                if path.is_dir() {
-                                    if filename.ends_with(".egg-info") {
-                                        Some(DistType::EggDir(path))
-                                    } else if filename.ends_with(".dist-info") {
-                                        Some(DistType::DistDir(path))
-                                    } else {
-                                        None
-                                    }
-                                } else if filename.ends_with(".egg-info") || filename.ends_with(".dist-info") {
-                                    Some(DistType::Info(path))
+                            if path.is_dir() {
+                                if filename.ends_with(".egg-info") {
+                                    Some(DistType::EggDir(path))
+                                } else if filename.ends_with(".dist-info") {
+                                    Some(DistType::DistDir(path))
                                 } else {
                                     None
                                 }
-                            })
-                            .collect()
-        };
-        package_dirs.extend(package_dir);
-    }
-    println!("{:?}", package_dirs);
-    package_dirs
+                            } else if filename.ends_with(".egg-info") || filename.ends_with(".dist-info") {
+                                Some(DistType::Info(path))
+                            } else {
+                                None
+                            }
+                        })
+                        .collect()
+        }
 }
 
 
@@ -70,7 +92,6 @@ fn get_dist_directories() -> Vec<String> {
                                             .filter(|s| s.contains("dist-packages"))
                                             .map(|s| s.trim().to_string())
                                             .collect();
-    println!("{:?}", dist_dirs);
     dist_dirs
 }
 
@@ -84,18 +105,36 @@ fn get_dist_directories() -> Vec<String> {
 // Requires-Dist: pexpect ; extra == 'test'
 // Requires-Dist: ruff ; extra == 'test'
 // Requires-Dist: wheel ; extra == 'test'
+fn parse_metadata(path: PathBuf) -> Metadata {
+    let mut requirements: Vec<String> = Vec::new();
+    let mut name: String = String::new();
 
-//     if let Ok(lines) = read_lines("./README.md") {
-//         for line in lines.map_while(Result::ok) {
-//             println!("{}", line);
-//         }
-//     }
-// }
+    let mut license: Vec<String> = Vec::new();
+    let mut license_classifier: Vec<String> = Vec::new();
+    if let Ok(lines) = read_lines(path) {
+        for line in lines.map_while(Result::ok) {
+            if line.starts_with("License: ") {
+                license.push(line);
+            } else if line.starts_with("Name: ") {
+                name = line;
+            } else if line.starts_with("Classifier: License :: OSI Approved :: ") {
+                license_classifier.push(line);
+            } else if line.starts_with("Requires-Dist: ") {
+                requirements.push(line);
+            }
+        }
+    }
+    if license.len() > license_classifier.len() {
+        Metadata {name, license, requirements}
+    } else {
+        Metadata {name, license: license_classifier, requirements}
+    }
+}
 
-// fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-// where
-//     P: AsRef<Path>,
-// {
-//     let file = File::open(filename)?;
-//     Ok(io::BufReader::new(file).lines())
-// }
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
