7c1a58d4be8e7283036af8409d7f8c519e46cfb6
nati b <nwtbekele@gmail.com>
2025-07-28 05:30:31 -0500

    add silent and dry run flags to formatter

--- src/argparse.rs
+++ src/argparse.rs
@@ -2,6 +2,8 @@ use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
+
+/// licensepy: Python project dependency license check and license header formating tool.
 pub struct Args {
     #[command(subcommand)]
     pub command: Commands,
@@ -30,23 +32,37 @@ pub enum Commands {
         /// Print only fail. Default if false.
         #[arg(short, long, default_value_t = false)]
         fail_print: bool,
+
+        /// Number of threads to use. Max is 32.
+        #[arg(short = 'j', long, default_value_t = 1)]
+        num_threads: u8,
     },
 
-    /// Run license header formatter on files.
+    /// Run license header formatter.
     Format {
-        /// Files to format
+        /// Files to run license header formatter on. If empty, will
+        /// search for all python source code files starting current directory
+        /// recursively exluding searches in directories starting with ".".
         #[arg()]
         files: Vec<String>,
 
-        /// Find requirements for each dependency. Default is false.
+        /// Licensee. Has precedence over value from config.
         #[arg(short, long)]
         licensee: Option<String>,
 
-        /// License year
+        /// License year. Has precedence over value from config.
         #[arg(short = 'y', long)]
         license_year: Option<u16>,
 
-        /// Number of threads to use
+        /// Don't print any outputs. Default if false.
+        #[arg(short, long, default_value_t = false)]
+        silent: bool,
+
+        /// Don't run formatter. Only print outputs. Default if false.
+        #[arg(short, long, default_value_t = false)]
+        dry_run: bool,
+
+        /// Number of threads to use. Default is 1. Max is 32.
         #[arg(short = 'j', long, default_value_t = 1)]
         num_threads: u8,
     },
